cfe9656ce95193f0b2e665093414e8598a594229
Rajan Khullar <rkhullar@nyit.edu>
2017-09-22 15:32:33 -0400

    added template for proj01

---
+++ proj01/Proj01.java
@@ -0,0 +1,19 @@
+package proj01;
+
+import javafx.application.Application;
+
+public class Proj01 extends Application
+{
+    private static int counter = 0;
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception
+    {
+        // your code
+        primaryStage.show();
+    }
+}
